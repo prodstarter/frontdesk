5b491a41222dbf6ba7f515cbc3bf26123499e8c4
Stanley Ojadovwa <stanley.warri@gmail.com>
2023-12-09 22:53:49 +0100

    Added autologin for demo

--- app/Filament/App/Pages/Auth/Login.php
+++ app/Filament/App/Pages/Auth/Login.php
@@ -7,4 +7,15 @@ use Filament\Pages\Auth\Login as BaseLogin;
 class Login extends BaseLogin
 {
     protected static string $view = 'filament.app.pages.auth.login';
+
+    public function mount(): void
+    {
+        parent::mount();
+
+        $this->form->fill([
+            'email' => 'admin@frontdesk.test',
+            'password' => '12345678',
+            'remember' => true,
+        ]);
+    }
 }
